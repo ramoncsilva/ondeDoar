ea82a11f21d4970e053b10cca54aca544f1688d0
Ramon Silva <ramondasilv@gmail.com>
2020-04-05 01:40:58 -0300

    adicionada uma versão minima da interface

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        
         <title>Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <style>
            body {
                 padding: 0;
@@ -18,11 +20,73 @@
                 height: 100%;
                 width: 100vw;
             }
+            #newPlaceButton{
+                position: absolute;
+                bottom: 0;
+                left: 0;
+            }
         </style>
     </head>
     <body>
         <div id="map"></div>
+        <button id="newPlaceButton" type="button" class="btn btn-primary">Cadastrar Local</button>
+        <div class="modal fade" id="saveModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Cadastro de Novo Local</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <input type="hidden" class="form-control" id="latitude">
+                    <input type="hidden" class="form-control" id="longitude">
+                    <div class="form-group">
+                        <label for="nome">Nome</label>
+                        <input type="text" class="form-control" id="nome">
+                    </div>
+                    <div class="form-group">
+                        <label for="rua">Rua</label>
+                        <input type="text" class="form-control" id="rua">
+                    </div>
+                    <div class="form-group">
+                        <label for="numero">Numero</label>
+                        <input type="text" class="form-control" id="numero">
+                    </div>
+                    <div class="form-group">
+                        <label for="adicional">Adicional</label>
+                        <input type="text" class="form-control" id="adicional">
+                    </div>
+                    <div class="form-group">
+                        <label for="bairro">Bairro</label>
+                        <input type="text" class="form-control" id="bairro">
+                    </div>
+                    <div class="form-group">
+                        <label for="cep">Cep</label>
+                        <input type="text" class="form-control" id="cep">
+                    </div>
+                    <div class="form-group">
+                        <label for="cep">Tipo de Doações</label>
+                        <select class="form-control">
+                            <option>Comida</option>
+                            <option>Higiene Pessoal</option>
+                            <option>Medicamentos</option>
+                            <option>Mascaras</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                    <button type="button" class="btn btn-primary">Salvar</button>
+                </div>
+                </div>
+            </div>
+        </div>
     </body>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script>
       var map;
       function initMap() {
@@ -31,11 +95,19 @@
           zoom: 13
         });
         map.setOptions({streetViewControl: false}); 
-        map.addListener('click', function(e) {
-            console.log(e)
+        map.addListener('click', function(event) {
+            
+            var latitude = event.latLng.lat();
+            var longitude = event.latLng.lng();
+            $('#latitude').val(latitude)
+            $('#longitude').val(longitude)
+            $('#saveModal').modal('show')
         });
 
       }
+      $("#newPlaceButton").click(function(){
+        $('#saveModal').modal('show')
+      })
     </script>
 
     <!--Load the API from the specified URL
@@ -44,7 +116,7 @@
     * The callback parameter executes the initMap() function
     -->
     <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=<MapsAPIKey>&callback=initMap">
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBKz9GkHBAI7vXMJQH4HQDZbgjIX8O0f84&callback=initMap">
     </script>
 
 </html>
